ed48984ff10daf61084c829a2f27be21c9ba50c0
Shashwat Kamboj <57861101+kambojshashwat@users.noreply.github.com>
2024-06-07 13:57:45 +0530

    Create Disk_Usage.sh
    This Script will help to check your Disk Usage based on Your Volume Size

---
+++ Disk_Usage.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+##################
+# AUTHOR : Shashwat
+# DATE : 07/06/2024
+#
+# This Script will check the Disk Usage of Your Linux Machine!!
+# VERSION : v1.0
+# ################
+
+echo "Checking Disk Usage"
+disk=`df -h | grep "8.0G" | awk '{print $5}' | cut -d '%' -f 1`
+
+if [ $disk -gt 80 ]; then
+        echo "ALERT: You disk size is $disk% , You need to Upgrade your Disk Size.."
+else
+        echo "No need to Worry, Disk Size is Under Control."
+fi
